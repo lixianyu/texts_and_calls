9f421f4018aef9d190a4e65fbd7bcd0defd26a9a
lixianyu <lbonus@163.com>
2018-01-25 10:48:52 +0800

    Finished Task2.

--- Task2.py
+++ Task2.py
@@ -3,13 +3,23 @@
 你将在以后的课程中了解更多有关读取文件的知识。
 """
 import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
 
 with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
+    dict = {}
+    for c in calls:
+        if c[0] in dict:
+            dict[c[0]] += int(c[-1])
+        else:
+            dict[c[0]] = int(c[-1])
+        if c[1] in dict:
+            dict[c[1]] += int(c[-1])
+        else:
+            dict[c[1]] = int(c[-1])
+    maxKey = max(dict, key = dict.get)
+    print("{} spent the longest time, {} seconds, on the phone during September 2016".format(maxKey, dict[maxKey]))
+
 
 """
 任务2: 哪个电话号码的通话总时间最长? 不要忘记，用于接听电话的时间也是通话时间的一部分。
@@ -22,4 +32,3 @@ September 2016.".
 如果键已经存在于字典内，为键所对应的值加上对应数值；
 如果键不存在于字典内，将此键加入字典，并将它的值设为给定值。
 """
-
