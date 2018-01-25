40400c7e841371d7e971664d3487bb72781f743f
lixianyu <lbonus@163.com>
2018-01-25 10:00:17 +0800

    Finished Task1

--- Task1.py
+++ Task1.py
@@ -3,17 +3,41 @@
 你将在以后的课程中了解更多有关读取文件的知识。
 """
 import csv
-with open('texts.csv', 'r') as f:
-    reader = csv.reader(f)
-    texts = list(reader)
 
-with open('calls.csv', 'r') as f:
-    reader = csv.reader(f)
-    calls = list(reader)
+def getSmsPhoneNumbers():
+    with open('texts.csv', 'r') as f:
+        reader = csv.reader(f)
+        #print(type(reader))
+        texts = list(reader)
+        #print(type(texts))
+        allPhoneNUM = []
+        for sms in texts:
+            if sms[0] not in allPhoneNUM:
+                allPhoneNUM.append(sms[0])
+            if sms[1] not in allPhoneNUM:
+                allPhoneNUM.append(sms[1])
+        return len(allPhoneNUM)
 
+def getCallPhoneNumbers():
+    with open('calls.csv', 'r') as f:
+        reader = csv.reader(f)
+        calls = list(reader)
+        allCallNUM = []
+        for call in calls:
+            if call[0] not in allCallNUM:
+                allCallNUM.append(call[0])
+            if call[1] not in allCallNUM:
+                allCallNUM.append(call[1])
+        return len(allCallNUM)
 
 """
 任务1：
 短信和通话记录中一共有多少电话号码？每个号码只统计一次。
 输出信息：
-"There are <count> different telephone numbers in the records.""""
+"There are <count> different telephone numbers in the records."
+"""
+smsCount = getSmsPhoneNumbers()
+# print(smsCount)
+callCount = getCallPhoneNumbers()
+# print(callCount)
+print("There are {} different telephone numbers in the records.".format(smsCount + callCount))
